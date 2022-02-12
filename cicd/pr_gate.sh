376259fc564b29e82234116de7f3dd422fce1442
Jeffrey Lipnick <jeffreylipnick@Jeffreys-MacBook-Pro.local>
2022-02-12 13:24:37 -0700

    Added checks.

--- cicd/pr_gate.sh
+++ cicd/pr_gate.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
-# Unfortunately there doesn't seem to be an easy way to all the go tests from the root
+# Unfortunately there doesn't seem to be an easy way to all the go tests from the root.
+# So this script is used to find all the directories with go.mod files and run the PR checks
+# from inside that directory.
+# Reference: https://stackoverflow.com/questions/9612090/how-to-loop-through-file-names-returned-by-find
 
-echo ran
+find . -name go.mod -execdir bash -c "go test" {} \;
