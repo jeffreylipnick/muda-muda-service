d9405bee86b71604cfc6a1dfa6e6139838aa4353
Jeffrey Lipnick <lipnickj@gmail.com>
2022-02-13 10:49:40 -0700

    Trying new pr checks.

--- main.go
+++ main.go
@@ -1,17 +1,19 @@
 package main // import "github.com/jeffreylipnick/muda-muda-service"
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 
-    "github.com/jeffreylipnick/muda-muda-service/api"
+	"github.com/jeffreylipnick/muda-muda-service/api"
 )
 
 func main() {
 
-    http.HandleFunc("/", api.HelloHandler)
 
-    fmt.Println("Server started at port 8080")
-    log.Fatal(http.ListenAndServe(":8080", nil))
+
+	http.HandleFunc("/", api.HelloHandler)
+
+	fmt.Println("Server started at port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
--- main_test.go
+++ main_test.go
@@ -4,4 +4,7 @@ import "testing"
 
 func TestMain(t *testing.T) {
 	t.Error("throwing error to test ci")
+
+
+	
 }
